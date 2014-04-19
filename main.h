c90af86ec3a1e0aac3f0b9ea03679aec5a29ff4c
humourless <humourless.conch@gmail.com>
2014-04-19 19:43:51 +0100

    created the main header and source file.

---
+++ main.h
@@ -0,0 +1,26 @@
+/*
+ *  EZDB main.h
+ *  Author: Gagondeep Srai
+ *  E-Mail: humourless.conch@gmail.com
+ *  Address: srai.io
+ *
+ *  For additional information go to:
+ *  github.com/humourless/EZDB
+ */
+
+#ifndef EZDB_main_h
+#define EZDB_main_h
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+
+
+#endif
+
